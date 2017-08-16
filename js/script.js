7f54338be011d8d2321585ac4cc1dace8f3095cd
HomaX1 <baitasova.anastasiia@gmail.com>
2017-08-16 22:58:21 +0300

    finished work

--- js/script.js
+++ js/script.js
@@ -2,23 +2,59 @@ $(function () {
     'use strict';
     const ALLCONST = {
         navLink: $('.navigation-link'),
-        menuClose: $('.sidebar__menu'),
+        sidebarMenuClose: $('.sidebar__menu'),
         sidebar: $('.sidebar'),
         headerPath: $('.path'),
-        contentMain: $('.main')
+        contentMain: $('.main'),
+        menuOpen: $('.path-link'),
+        menuClose: $('.sidebar-link')
     };
 
 
     let browserSize = () => {
-            if (document.documentElement.clientWidth <= 1040) {
-                ALLCONST.sidebar.addClass('sidebar_min-width');
-            } else if (document.documentElement.clientWidth > 1040) {
-                ALLCONST.sidebar.removeClass('sidebar_min-width');
-            }
+        if (document.documentElement.clientWidth <= 1040) {
+            ALLCONST.sidebar.addClass('sidebar_min-width');
+        } else if (document.documentElement.clientWidth > 1040) {
+            ALLCONST.sidebar.removeClass('sidebar_min-width');
+        }
     };
-    $(window).resize(browserSize);
-    browserSize();
 
+    let showMinMenu = () => {
+        if (!ALLCONST.sidebar.hasClass('sidebar_min-width')) {
+            ALLCONST.sidebarMenuClose.hide();
+
+            ALLCONST.headerPath.animate({'margin-left': '160px'}, 600);
+            ALLCONST.contentMain.animate({'margin-left': '120px'}, 600);
+            ALLCONST.sidebar.animate({left: '-315px'}, 600, () => {
+                ALLCONST.sidebar.addClass('sidebar_min-width').animate({left: '0', width: '120px'}, 700, () => {
+                    ALLCONST.sidebarMenuClose.show();
+                });
+            });
+
+        } else {
+            ALLCONST.sidebarMenuClose.hide();
+
+            ALLCONST.sidebar.animate({left: '-315px'}, 700, () => {
+                ALLCONST.sidebar.removeClass('sidebar_min-width').animate({left: '0', width: '315px'}, 800, () => {
+                    ALLCONST.sidebarMenuClose.show();
+                });
+                ALLCONST.headerPath.animate({'margin-left': '355px'}, 800);
+                ALLCONST.contentMain.animate({'margin-left': '315px'}, 800);
+            });
+        }
+    };
+
+    let showMobileMenu = () => {
+        ALLCONST.sidebar.animate({opacity: '1'}, () => {
+            ALLCONST.sidebar.removeClass('sidebar_min-width').addClass('sidebar_mobile').fadeIn();
+        });
+    };
+
+    let closeMobileMenu = () => {
+        ALLCONST.sidebar.animate({opacity: '0'}, 400, () => {
+            ALLCONST.sidebar.removeClass('sidebar_min-width sidebar_mobile').fadeOut();
+        });
+    };
 
     ALLCONST.navLink.click(function () {
         if ($(this).hasClass('navigation-link_active')) {
@@ -33,32 +69,11 @@ $(function () {
         }
     });
 
+    ALLCONST.sidebarMenuClose.click(showMinMenu);
+    ALLCONST.menuOpen.click(showMobileMenu);
+    ALLCONST.menuClose.click(closeMobileMenu);
+    $(window).resize(browserSize);
 
-    let showMinMenu = () => {
-        if (!ALLCONST.sidebar.hasClass('sidebar_min-width')) {
-            ALLCONST.menuClose.hide();
-
-            ALLCONST.headerPath.animate({'margin-left': '160px'}, 600);
-            ALLCONST.contentMain.animate({'margin-left': '120px'}, 600);
-            ALLCONST.sidebar.animate({left: '-315px'}, 600, () => {
-                ALLCONST.sidebar.addClass('sidebar_min-width').animate({left: '0', width: '120px'}, 700, () => {
-                    ALLCONST.menuClose.show();
-                });
-            });
-
-        } else {
-            ALLCONST.menuClose.hide();
-
-            ALLCONST.sidebar.animate({left: '-315px'}, 700, () => {
-                ALLCONST.sidebar.removeClass('sidebar_min-width').animate({left: '0', width: '315px'}, 800, () => {
-                    ALLCONST.menuClose.show();
-                });
-                ALLCONST.headerPath.animate({'margin-left': '355px'}, 800);
-                ALLCONST.contentMain.animate({'margin-left': '315px'}, 800);
-            });
-        }
-    };
-
-    ALLCONST.menuClose.click(showMinMenu);
+    browserSize();
 
 });
---
+++ readme.md
@@ -0,0 +1,34 @@
+# Test Project For Diversido Mobile
+
+## Technologies, which I used:
+* HTML
+* CSS (SCSS)
+* JavaScript
+* jQuery
+* jQuery-mobile
+* Gulp
+* Responsive design
+* BEM methodology
+
+## Instruction
+
+NodeJS version >= 6.9.1
+
+
+Install all modules:
+```
+npm install
+```
+
+You could install gulp if you don't have it:
+
+```
+npm install -g gulp
+```
+
+Run server:
+
+```
+gulp connect
+```
+Open in browser: http://localhost:8080
